626fbb935ceac8fcb223bfac66ac978fbc4d7fde
parthivii <parthivisrivastava14@gmail.com>
2020-09-17 18:44:19 +0530

    Updates

---
+++ precache-manifest.4233b676a1aa5affaa8b53a468366094.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eab9b8a5778cf3c57328ff4bb368925",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "ce8dadbc99e0eaa2a52e",
+    "url": "/react-todo/static/css/main.a99200be.chunk.css"
+  },
+  {
+    "revision": "21a4f525852d8c3e35fb",
+    "url": "/react-todo/static/js/2.c17eaa80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.c17eaa80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce8dadbc99e0eaa2a52e",
+    "url": "/react-todo/static/js/main.4b772a66.chunk.js"
+  },
+  {
+    "revision": "8b60092a6ae08cc228b0",
+    "url": "/react-todo/static/js/runtime-main.40cbbb06.js"
+  }
+]);
